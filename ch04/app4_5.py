58731d1332f8460725293c61bc8c1f5a81127196
wfsiew <siewwingfei@hotmail.com>
2013-11-21 23:50:57 +0800

    commit

---
+++ ch04/app4_5.py
@@ -0,0 +1,27 @@
+'''
+Created on Nov 21, 2013
+
+@author: wfsiew
+'''
+
+def get_sum(x):
+    v = 0
+    for i in range(1, x):
+        a = x % i
+        if a == 0:
+            v = v + i
+            
+    return v
+
+def is_prime(x):
+    s = get_sum(x)
+    if x == s:
+        return True
+    
+    else:
+        return False
+
+if __name__ == '__main__':
+    for i in range(1, 1001):
+        if is_prime(i):
+            print i
